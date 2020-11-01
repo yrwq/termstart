10d9e50350715853dc70bdbb7632688a5c216e28
BuildTools <unconfigured@null.spigotmc.org>
2020-11-01 22:41:29 +0100

    separate js

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 <h2 align="center"> Termstart </h2>
 
+<h3 align="center"> what is this? </h3>
+
+<h5 align="center"> 
+
+This is a terminal like website, that you can use as a startpage in any browser <br>
+You can type in the first letter of the website you wanna go to, <br>
+type in the whole name of the website, or just simply click it. <br>
+Feel free to open issues, suggesting features or other things!
+
+</h5>
 
---
+++ js/sites.js
@@ -0,0 +1,75 @@
+var pr = {
+	0: "r",
+	1: "re",
+	2: 'red', 
+	3: 'reddit'
+};
+
+var pg = {
+	0: "g",
+	1: "gi",
+	2: "git",
+	3: "github"
+};
+
+var pu = {
+	0: "u",
+	1: "un",
+	2: "uns",
+	3: "unsplash"
+}
+
+var pc = {
+	0: "c",
+	1: "ch",
+	2: "ches"
+	3: "chess",
+}
+
+function search(ele) {
+	if(event.key === 'Enter') {
+		if( 
+			ele.value == pg[0] ||
+			ele.value == pg[1] ||
+			ele.value == pg[2] ||
+			ele.value == pg[3] 
+		){ window.open("https://github.com", '_blank'); } 
+		else if(
+			ele.value == pu[0] ||
+			ele.value == pu[1] ||
+			ele.value == pu[2] ||
+			ele.value == pu[3] 
+		{ window.open("https://unsplash.com", '_blank'); } 
+		else if(
+			ele.value == pc[0] ||
+			ele.value == pc[1] ||
+			ele.value == pc[2] ||
+			ele.value == pc[3] 
+		){ window.open("https://chess.com", '_blank'); } 
+		else if(
+			ele.value == pr[0] ||
+			ele.value == pr[1] ||
+			ele.value == pr[2] ||
+			ele.value == pr[3] 
+		){ window.open("https://reddit.com", '_blank'); }
+	}
+}
+
+function getTime() {
+	let date = new Date(),
+        min = date.getMinutes(),
+        hour = date.getHours();
+    
+        return '' + 
+        	(hour < 10 ? ('0' + hour) : hour) + ':' + 
+                (min < 10 ? ('0' + min) : min);
+}
+
+window.onload = () => {
+	document.getElementById('clock').innerHTML = getTime();
+        		
+        setInterval( () => {
+        	document.getElementById('clock').innerHTML = getTime();
+        },100);
+}
+
