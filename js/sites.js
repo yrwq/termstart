2ed51347d5f44cdfb1a98fcbdc36cb2764af75cf
yrwq <yrwqid@gmail.com>
2020-11-06 02:17:41 +0100

    optimize search

--- js/sites.js
+++ js/sites.js
@@ -1,65 +1,29 @@
-var pr = {
-	0: "r",
-	1: "re",
-	2: 'red', 
-	3: 'reddit'
-};
-
-var pg = {
-	0: "g",
-	1: "gi",
-	2: "git",
-	3: "github"
-};
-
-var pu = {
-	0: "u",
-	1: "un",
-	2: "uns",
-	3: "unsplash"
-}
-
-var pc = {
-	0: "c",
-	1: "ch",
-	2: "ches",
-	3: "chess"
-}
-
 function search(ele) {
 	if(event.key === 'Enter') {
-		if( 
-			ele.value == pg[0] ||
-			ele.value == pg[1] ||
-			ele.value == pg[2] ||
-			ele.value == pg[3] 
-		){
+        // github
+		if( ele.value.includes("git") == true ){
 			document.getElementById('search').value = '';
-			window.open("https://github.com", '_blank'); } 
+			window.open("https://github.com", '_blank');
+        }
+        // unsplash
 		else if(
-			ele.value == pu[0] ||
-			ele.value == pu[1] ||
-			ele.value == pu[2] ||
-			ele.value == pu[3] 
-		){
+            ele.value.includes("uns") == true ){
 			document.getElementById('search').value = '';
-			window.open("https://unsplash.com", '_blank'); } 
+			window.open("https://unsplash.com", '_blank');
+        }
+        // chess
 		else if(
-			ele.value == pc[0] ||
-			ele.value == pc[1] ||
-			ele.value == pc[2] ||
-			ele.value == pc[3] 
-		){ 
+            ele.value.includes("che") == true ){
 			document.getElementById('search').value = '';
-			window.open("https://chess.com", '_blank'); } 
+			window.open("https://chess.com", '_blank');
+        }
+        // reddit
 		else if(
-			ele.value == pr[0] ||
-			ele.value == pr[1] ||
-			ele.value == pr[2] ||
-			ele.value == pr[3] 
-		){
+            ele.value.includes("red") == true ){
 			document.getElementById('search').value = '';
-			window.open("https://reddit.com", '_blank'); }
+			window.open("https://reddit.com", '_blank');
+        }
+        // search with duckduckgo
 		else if(ele.value.includes("search") == true) {
 			let doSearch = ele.value.replace("search", "");
 			window.open("https://duckduckgo.com/?q=" + doSearch);
