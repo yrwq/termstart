0cff8cb5f1a678c7f9d29cff0684e31418e15f24
yrwq <yrwqid@gmail.com>
2020-12-15 10:25:21 +0100

    fix focus

--- js/helpers.js
+++ js/helpers.js
@@ -1,24 +1,24 @@
 function getTime() {
-	let date = new Date(),
+    let date = new Date(),
         min = date.getMinutes(),
         hour = date.getHours();
-
-        return '' +
-        	(hour < 10 ? ('0' + hour) : hour) + ':' +
-                (min < 10 ? ('0' + min) : min);
+    
+    return '' +
+        (hour < 10 ? ('0' + hour) : hour) + ':' +
+        (min < 10 ? ('0' + min) : min);
 }
 
 window.onload = () => {
     document.getElementById("clock").innerHTML = getTime();
     document.getElementById("prompt-input").focus();
-
+    
     // Focus prompt if Enter pressed
     addEventListener("keydown", function(event) {
 	if (event.keyCode == 13 || 32) {
 	    document.getElementById("prompt-input").focus();
 	}
     });
-
+    
     setInterval( () => {
         document.getElementById("clock").innerHTML = getTime();
     },100);
