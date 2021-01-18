98ffacee2d0c28e1fdf51f42869f81f872394abb
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-18 21:38:40 +0100

    create commands.js

---
+++ js/commands.js
@@ -0,0 +1,35 @@
+const ls = () => {
+
+}
+
+const add = (name, url) => {
+
+}
+
+const del = (name) => {
+
+}
+
+const open = (url) => {
+
+}
+
+const search = (string) => {
+
+}
+
+const help = (name) => {
+
+}
+
+const clear = () => {
+
+}
+
+const themes = () => {
+
+}
+
+const theme = (theme) => {
+
+}
